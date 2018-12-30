8e964d1cf4947aeeacc3cdc4c26ab05a29304739
Onur Kilic <onurkilic5@gmail.com>
2018-12-30 20:13:37 +0300

    Shortened the js code and changed the sorting of css and js

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-function hr() {
+function time() {
 	let d = new Date();
 	let h = d.getHours();
 	let m = d.getMinutes();
+	let s = d.getSeconds();
+
 	let hourhand = document.querySelector(".hour");
 	let hour = (h / 12) * 360 + 90 + (m / 2);
 	hourhand.style.transform = `rotate(${hour}deg)`;
-}
 
-function mn() {
-	let d = new Date();
-	let m = d.getMinutes();
 	let minutehand = document.querySelector(".minute");
 	let minute = (m / 60) * 360 + 90;
 	minutehand.style.transform = `rotate(${minute}deg)`;
-}
 
-function scn() {
-	let d = new Date();
-	let s = d.getSeconds();
 	let second = (s / 60) * 360 + 90;
 	let secondhand = document.querySelector(".second");
 	secondhand.style.transform = `rotate(${second}deg)`;
 }
 
-setInterval(scn, 1000);
-setInterval(hr, 1000);
-setInterval(mn, 1000);
+window.addEventListener("load", time);
+
+setInterval(time, 1000);
